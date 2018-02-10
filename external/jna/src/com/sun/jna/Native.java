4f4aa49088c8acc01d36b90a8bdcfc8c7774432f
Fred Gotwald <fred.gotwald@veriktig.com>
2018-02-10 14:32:31 -0800

    Add modified files to jna.

--- external/jna/src/com/sun/jna/Native.java
+++ external/jna/src/com/sun/jna/Native.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Veriktig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /* Copyright (c) 2007-2015 Timothy Wall, All Rights Reserved
  *
  * The contents of this file is dual-licensed under 2 
@@ -1599,9 +1615,11 @@ public final class Native implements Version {
             }
             return CVT_NATIVE_MAPPED;
         }
+        /*
         if (JNIEnv.class == type) {
             return CVT_JNIENV;
         }
+        */
         return allowObjects ? CVT_OBJECT : CVT_UNSUPPORTED;
     }
 
